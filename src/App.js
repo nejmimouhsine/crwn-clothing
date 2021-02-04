2694548cb60de4781e1dd05bec0a72028dd49f72
nejmimouhsine <nejmi.mouhcine10@gmail.com>
2021-02-04 18:35:30 +0100

    update our items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.page';
+
+const HatsPage = () => <h1>Hats Page</h1>;
+const JacketPage = () => <h1>Jacket Page</h1>;
 
 const App = () => {
 	return (
 		<div className="App">
-			<HomePage />
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route path="/hats" component={HatsPage} />
+				<Route path="/jacket" component={JacketPage} />
+			</Switch>
 		</div>
 	);
 };
